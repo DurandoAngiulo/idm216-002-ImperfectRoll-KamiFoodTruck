b328c9cad7883f33e964e783ea6f0b6c8561933d
DurandoAngiulo <87902832+DurandoAngiulo@users.noreply.github.com>
2023-02-01 14:33:05 -0500

    fixed favicon typo

--- final/_components/header-kami.php
+++ final/_components/header-kami.php
@@ -15,7 +15,7 @@ $document_title = $page_title . ' | ' . $site_title; // Home | J
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" type="image/x-icon" href="<?php echo site_url(); ?>/dist/images/kami-Fav-Icon.png">
+  <link rel="icon" type="image/x-icon" href="<?php echo site_url(); ?>/dist/images/Kami-Fav-Icon.png">
   <!-- <script src="https://cdn.tiny.cloud/1/x2yk3gj8du2d3si372pt1ylmp1le9s0s2fns9fj75nflr46d/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script> -->
   <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
