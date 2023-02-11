e50b649b6cb092c396be52361ea82dd3deda2764
DurandoAngiulo <87902832+DurandoAngiulo@users.noreply.github.com>
2023-02-11 14:21:58 -0500

    updated directory with new page stylesheet

---
+++ final/pages/home.php
@@ -0,0 +1,12 @@
+<?php
+include_once __DIR__ . '/../app.php';
+$page_title = 'Critical Path | KAMI Food Truck';
+include_once __DIR__ . '/../_components/header-kami.php';
+?>
+<body class="sitebg">
+    <!-- wrapper -->
+   
+    
+
+    <?php include_once __DIR__ . '/../_components/footer.php';
+?>
