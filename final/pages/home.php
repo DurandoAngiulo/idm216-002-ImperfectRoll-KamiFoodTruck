2fc04d6bc2ba8235f702d48a834079d8938e24cd
DurandoAngiulo <87902832+DurandoAngiulo@users.noreply.github.com>
2023-02-11 15:25:15 -0500

    made mobile nav

--- final/pages/home.php
+++ final/pages/home.php
@@ -1,9 +1,12 @@
 <?php
 include_once __DIR__ . '/../app.php';
-$page_title = 'Critical Path | KAMI Food Truck';
+$page_title = 'Home | KAMI Food Truck';
 include_once __DIR__ . '/../_components/header-kami.php';
 ?>
 <body class="sitebg">
+<?php 
+include_once __DIR__ . '/../_components/navbar-mobile.php';
+?>
     <!-- wrapper -->
    
     
