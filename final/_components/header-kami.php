748869521e3643c75cd869ee949d187f004cf7fc
DurandoAngiulo <87902832+DurandoAngiulo@users.noreply.github.com>
2023-02-12 14:36:58 -0500

    other tweaks to fix errors

--- final/_components/header-kami.php
+++ final/_components/header-kami.php
@@ -25,5 +25,6 @@ $document_title = $page_title . ' | ' . $site_title; // Home | J
       crossorigin="anonymous"
     />
   <link rel="stylesheet" href="<?php echo site_url(); ?>/dist/styles/main.css">
+  <link rel="stylesheet" href="<?php echo site_url(); ?>/dist/styles/criticalPath.css">
   <title><?php echo $document_title ; ?></title>
 </head>
