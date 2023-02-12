31a006b45b89576b3b68c57b4d2157132ef0e432
DurandoAngiulo <87902832+DurandoAngiulo@users.noreply.github.com>
2023-02-12 14:03:37 -0500

    added home css page

--- final/pages/home.php
+++ final/pages/home.php
@@ -7,8 +7,12 @@ include_once __DIR__ . '/../_components/header-kami.php';
 <?php 
 include_once __DIR__ . '/../_components/navbar-mobile.php';
 ?>
+<style><?php 
+include_once __DIR__ . '/../dist/styles/home.css';
+?></style>
+
     <!-- wrapper -->
-   
+   <h2 class="red">test</h2>
     
 
     <?php include_once __DIR__ . '/../_components/footer.php';
