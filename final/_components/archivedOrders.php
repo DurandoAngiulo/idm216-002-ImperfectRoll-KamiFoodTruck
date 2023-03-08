3e5009e56173cb2ea9572cabb6810b42b5cb5f88
DurandoAngiulo <87902832+DurandoAngiulo@users.noreply.github.com>
2023-03-08 14:38:14 -0500

    quick fixes

--- final/_components/archivedOrders.php
+++ final/_components/archivedOrders.php
@@ -16,19 +16,11 @@ while ($item = mysqli_fetch_array($cart_items)) {
         </div>
         <div class='d-flex flex-row justify-content-between align-items-center mb-2'>
             <p>Total</p>
-            <p>$14.16</p>
+            <p>{$item['fin']}</p>
         </div>
 
         <div class='heading-border d-flex justify-content-between mb-3'></div>
-
-        <div class='d-flex flex-row justify-content-between align-items-center mb-2'>
-            <p>1 Bibimbap</p>
-            <p>$10</p>
-        </div>
-        <div class=d-flex flex-row justify-content-between align-items-center mb-2'>
-            <p>2 Kimchi</p>
-            <p>$2</p>
-        </div>
+        include __DIR__ . '/checkoutItem.php';
         <button onclick='' type='button' class='btn btn-primary p-2'>View QR Code</button>
 
     </div>
