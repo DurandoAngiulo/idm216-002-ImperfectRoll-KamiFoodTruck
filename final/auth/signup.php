02a10af0ae925a2e90af992c33d4680cf196f491
jiitra <74521649+jiitra@users.noreply.github.com>
2023-03-13 20:54:56 -0400

    all responsive

--- final/auth/signup.php
+++ final/auth/signup.php
@@ -5,13 +5,14 @@ include_once __DIR__ . '/../_components/header-kami.php';
 ?>
 <body class="backgroundColor">
 
+<div class="container">
 <div class="mt-4">
    <img class="kami-duck-large mx-auto d-block" src="<?php echo site_url(); ?>/dist/images/kami-duck.png">
    <h1 class="logoFont text-center mb-3">KAMI</h1>
 </div>
 
-   <div class="d-flex justify-content-center">
-        <form  class="d-flex justify-content-center flex-wrap" action="<?php echo site_url();?>/_includes/process-signup.php" method="POST">
+   <div class="input-container justify-content-center">
+        <form  class="d-flex flex-column justify-content-center flex-wrap" action="<?php echo site_url();?>/_includes/process-signup.php" method="POST">
             <p>
             <label for="name" class="mb-1">First Name</label></br>
             <input type="text" id="name"  class="mb-2 p-2 input-login" name="first_name"></p>
@@ -35,5 +36,6 @@ include_once __DIR__ . '/../_components/header-kami.php';
             </div>
         </form>
     </div>
+</div>
 
     
--- final/order-confirmation.php
+++ final/order-confirmation.php
@@ -7,7 +7,7 @@ include_once __DIR__ . '/_components/header-kami.php';
 
 <div class="container">
 <button onclick="window.location.href = '<?php echo site_url(); ?>/index.php';"type="button" 
-class="btn-close m-3 position-absolute top-0 end-0" aria-label="Close"></button>
+class="btn-close m-3 position-absolute top-0" aria-label="Close"></button>
 
 <h2 class="logoFontSm text-center mt-3">KAMI</h2>
 <div class="page mx-3">
